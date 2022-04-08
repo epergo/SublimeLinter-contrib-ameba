ae91c4724e5e2a53ee1fb72d9dd58a3da47f9623
Troy Sornson <troy@sornson.io>
2022-04-08 10:57:22 -0600

    Be more flexible on types of problems, add optional auto_fix setting

--- linter.py
+++ linter.py
@@ -2,16 +2,20 @@ from SublimeLinter.lint import Linter
 
 
 class Ameba(Linter):
-    regex = (r'^.+:(?P<line>\d+):(?P<col>\d+): ((?P<error>E)|(?P<warning>W|C)): (?P<message>.+)$')
+    regex = (r'^.+:(?P<line>\d+):(?P<col>\d+): ((?P<error>E)|(?P<warning>.)): (?P<message>.+)$')
     multiline = False
 
     tempfile_suffix = '-'
 
     defaults = {
         'selector': 'source.crystal',
+        'auto_fix': false,
         'executable': '${folder}/bin/ameba'
     }
 
     def cmd(self):
         settings = self.get_view_settings()
-        return (settings['executable'], '--format', 'flycheck', '${file}')
+        if settings['auto_fix']:
+            return (settings['executable'], '--format', 'flycheck', '--fix', '${file}')
+        else:
+            return (settings['executable'], '--format', 'flycheck', '${file}')
