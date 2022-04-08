bc8c05542473faf58b0ce21137d5268415b75d95
Troy Sornson <troy@sornson.io>
2022-04-08 10:52:54 -0600

    Support correctable problems as warnings

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter
 
 
 class Ameba(Linter):
-    regex = (r'^.+:(?P<line>\d+):(?P<col>\d+): ((?P<error>E)|(?P<warning>W)): (?P<message>.+)$')
+    regex = (r'^.+:(?P<line>\d+):(?P<col>\d+): ((?P<error>E)|(?P<warning>W|C)): (?P<message>.+)$')
     multiline = False
 
     tempfile_suffix = '-'
